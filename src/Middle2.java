abd35242b36f5ff2a1876a65d2228112252eb09b
Neto test <uv9058390@gmail.com>
2025-05-20 10:58:13 +0300

    initial commit correct

--- src/Middle2.java
+++ src/Middle2.java
@@ -4,25 +4,27 @@ public class Middle2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите количество элементов массива (от 1 до 100):");
-        int N = scanner.nextInt();
-        if (N < 1 || N > 100) {
+        int size = scanner.nextInt();
+        if (size < 1 || size > 100) {
             System.out.println("Некорректный размер массива. Размер должен быть от 1 до 100.");
             return;
         }
-        double[] numbers = new double[N];
-        System.out.println("Введите " + N + " вещественных чисел:");
-        for (int i = 0; i < N; i++) {
+        double[] numbers = new double[size];
+        System.out.println("Введите " + size + " вещественных чисел:");
+        for (int i = 0; i < size; i++) {
             numbers[i] = scanner.nextDouble();
         }
-        double maxAbs = Math.abs(numbers[0]);
-        double maxElement = numbers[0];
-        for (int i = 1; i < N; i++) {
+        //double maxAbs = Math.abs(numbers[0]);
+        double maxAbs = 0;
+        //double maxElement = numbers[0];
+        for (int i = 1; i < size; i++) {
             double absValue = Math.abs(numbers[i]);
             if (absValue > maxAbs) {
-                maxAbs = absValue;
-                maxElement = numbers[i];
+             // maxAbs = absValue;
+                maxAbs = Math.max(Math.abs(numbers[i]), maxAbs);
+                //maxElement = numbers[i];
             }
         }
-        System.out.println("Результат: " + maxElement);
+        System.out.println("Результат: " + maxAbs);
     }
 }
